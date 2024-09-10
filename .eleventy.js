d7e75de0199c1f05c7fe48136d5040c9fdd6f7c9
Cory Streiff <cory@ragnarson.com>
2024-09-10 12:34:09 +0200

    Change portrait image and add footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.js");
-
   eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy("cory_streiff_resume.pdf");
+
   eleventyConfig.addWatchTarget("styles.css");
 };
