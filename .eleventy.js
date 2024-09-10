44164c24a8ec011a79a7104c2888b5cc4f302995
Cory Streiff <cory@ragnarson.com>
2024-09-10 14:29:35 +0200

    Add favicon, fix mobile header styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("cory_streiff_resume.pdf");
+  eleventyConfig.addPassthroughCopy({ "img/favicon": "/" });
 
   eleventyConfig.addWatchTarget("styles.css");
 };
