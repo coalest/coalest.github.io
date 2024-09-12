5dfdf49ff461148a85823b251869107509796d69
Cory Streiff <cory@ragnarson.com>
2024-09-12 12:52:37 +0200

    Add back sticky navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("bundle.js");
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("cory_streiff_resume.pdf");
   eleventyConfig.addPassthroughCopy({ "img/favicon": "/" });
--- _site/.eleventy.js
+++ _site/.eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.js");
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("cory_streiff_resume.pdf");
+  eleventyConfig.addPassthroughCopy({ "img/favicon": "/" });
 
   eleventyConfig.addWatchTarget("styles.css");
 };
