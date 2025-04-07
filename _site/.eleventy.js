e8aa7dfac5f8ddada2096d9b01e9c8e99a76d7cb
Cory Streiff <c.streiff@pm.me>
2025-04-07 14:27:51 +0200

    Add _site and node_modules to gitignore

--- _site/.eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("bundle.js");
-  eleventyConfig.addPassthroughCopy("styles.css");
-  eleventyConfig.addPassthroughCopy("cory_streiff_resume.pdf");
-  eleventyConfig.addPassthroughCopy({ "img/favicon": "/" });
-
-  eleventyConfig.addWatchTarget("styles.css");
-};
